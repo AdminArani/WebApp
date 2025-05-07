7591b2afdbed784ae51e0c9e1a5cba665bd61b61
Rolando Mejia <rolando.mejia@arani.hn>
2025-05-06 20:16:48 -0600

    antes del cambio a modular

--- src/config.js
+++
@@ -1,9 +0,0 @@
-// Definir primero el objeto config
-const config = {
-    apiUrl: 'https://app.arani.hn',
-  };
-  
-  // Luego, usar la propiedad apiUrl de config para definir REACT_APP_API_URL
-  const REACT_APP_API_URL = config.apiUrl;
-  
-  export default config;
